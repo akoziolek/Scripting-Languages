0b77f7f59848f478577fe78ef9460248d8c5c874
Koziolek <koziolola43@gmail.com>
2025-03-24 15:22:00 +0100

    Rebased onto new main

---
+++ Lab3/log_filter_by_code.py
@@ -0,0 +1,11 @@
+#HTTP codes
+#1xx informational response – the request was received, continuing process
+#2xx successful – the request was successfully received, understood, and accepted
+#3xx redirection – further action needs to be taken in order to complete the request
+#4xx client error – the request contains bad syntax or cannot be fulfilled
+#5xx server error – the server failed to fulfil an apparently valid request
+
+def get_entries_by_code(log, HTTP_code):
+    if not isinstance(HTTP_code, int) and (HTTP_code < 100 or HTTP_code > 599):
+        raise ValueError('Invalid HTTP code')
+    return log.filter(lambda row: row[6] == HTTP_code)
