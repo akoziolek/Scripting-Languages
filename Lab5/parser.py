e6b421f0513d3c284d316d783384cad83cdd9b69
emilia <emilia@kosior.pl>
2025-05-29 18:23:40 +0200

    parser fixes that disappeared

--- Lab5/parser.py
+++ Lab5/parser.py
@@ -1,10 +1,11 @@
 import csv
+import re
+
 import console_logger, logging
 import os
 from pathlib import Path
 from os import listdir
 import datetime
-
 
 logger = logging.getLogger(__name__)
 
@@ -29,27 +30,30 @@ def validate_path(file_path, format, enable_logging=False):
     if not os.path.exists(file_path):
         if enable_logging: logger.error(f'Path "{file_path}" doesn\'t exist')
         raise FileNotFoundError(f'Path {file_path} doesn\'t exist')
-    
+
     if not os.path.isfile(file_path):
         if enable_logging: logger.error(f'"{file_path}" is not a file')
         raise FileNotFoundError(f'"{file_path}" is not a file')
-    
+
     if os.path.splitext(file_path)[1] != format:
         if enable_logging: logger.error(f'"{file_path}" is not an excepted type of: {type}')
         raise FileExistsError(f'"{file_path}" is not an excepted type of: {type}')
+
 
 def log_read_bytes(line, enable_logging=False):
     if enable_logging:
         logger.debug(f'Read line bytes: {len(line)}')
 
+
 def convert_to_csv_line(row):
     return (','.join(row) + '\n').encode('utf-8')
+
 
 def parse_data(path, enable_logging=False):
     validate_path(path, '.csv', True)
 
     try:
-        with open (path, 'r', encoding='UTF-8') as file:
+        with open(path, 'r', encoding='UTF-8') as file:
             if enable_logging: logger.info(f'File "{file.name}" has been opened')
 
             reader = csv.reader(file)
@@ -57,35 +61,41 @@ def parse_data(path, enable_logging=False):
             line = next(reader)
 
             for i in range(1, len(line)):
-                loglist.append({line[0] : line[i]})
+                loglist.append({line[0]: line[i]})
 
             log_read_bytes(convert_to_csv_line(line), enable_logging)
 
-            for i in range(5):
-                line = next(reader)
+            line = next(reader)
+            datepattern = re.compile(r'^\d{2}/\d{2}/\d{2} \d{2}:\d{2}$')
+            while not re.match(datepattern, line[0]):
                 log_read_bytes(convert_to_csv_line(line), enable_logging)
-                for j in range(1,len(line)):
-                    loglist[j-1].update({line[0] : line[j]})
+                for j in range(1, len(line)):
+                    loglist[j - 1].update({line[0]: line[j]})
+                line = next(reader)
 
             for dic in loglist:
-                dic.update({'Pomiary' : {}})
+                dic.update({'Pomiary': {}})
 
-            for line in reader:
+            def handle_measurements_line(line):
                 log_read_bytes(convert_to_csv_line(line), enable_logging)
                 date = line[0]
                 if date in loglist[0]['Pomiary']:
                     date = datetime.strptime(date, '%m/%d/%y %H:%M')
                     date += datetime.timedelta(hours=12)
                     date = date.strftime('%m/%d/%y %H:%M')
-                for i in range(1,len(line)):
-                    loglist[i-1]['Pomiary'].update({date : line[i]})
+                for i in range(1, len(line)):
+                    loglist[i - 1]['Pomiary'].update({date: line[i]})
+
+            handle_measurements_line(line)
+            for line in reader:
+                handle_measurements_line(line)
 
             for line in reader:
                 log_read_bytes(convert_to_csv_line(line), enable_logging)
 
-                for i in range(1,len(line)):
-                    loglist[i-1]['Pomiary'].update({line[0] : line[i]})
-            
+                for i in range(1, len(line)):
+                    loglist[i - 1]['Pomiary'].update({line[0]: line[i]})
+
 
     except:
         if enable_logging: logger.error(f'An error occured while reading "{path}" file')
@@ -93,12 +103,12 @@ def parse_data(path, enable_logging=False):
     finally:
         if enable_logging: logger.info(f'File :{file.name}" has been closed')
 
-
     return loglist
     # Lista słowników, z czego każdy słownik zawiera dane jednej stacji - klucz nazwa zmiennej wartość wartość,
     # pomiary są jedną z tych zmiennych o nazwie "Pomiary", wartość jest słownikiem klucz data wartość wynik pomiaru
 
-def parse_metadata(path, enable_logging=False, as_dict=False,) -> dict | list:
+
+def parse_metadata(path, enable_logging=False, as_dict=False, ):
     validate_path(path, '.csv', enable_logging)
 
     try:
@@ -125,12 +135,11 @@ def parse_metadata(path, enable_logging=False, as_dict=False,) -> dict | list:
     except Exception as e:
         if enable_logging:
             logger.error(f'An error occurred while reading "{path}" file: {e}')
-        raise 
+        raise
 
     finally:
         if enable_logging:
             logger.info(f'File "{path}" has been closed')
-
 
 
 if __name__ == '__main__':
