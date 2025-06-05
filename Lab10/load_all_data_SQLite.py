b815ed469754dc09f385da6235b2d6cdecdeda19
emilia <emilia@kosior.pl>
2025-06-05 23:53:57 +0200

    corrected subprocess call

--- Lab10/load_all_data_SQLite.py
+++ Lab10/load_all_data_SQLite.py
@@ -5,7 +5,7 @@ def add_all_data(database_name):
     for i in range(1, 13):
         index = f'{i:02}'
         filename = f'data/historia_przejazdow_2021-{index}.csv'
-        subprocess.run(['python',b'load_data.py', filename, f'{database_name}',], check=True)
+        subprocess.run(['python','load_data_SQLite.py', filename, f'{database_name}',], check=True)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Load history of all rentals into given database.')
